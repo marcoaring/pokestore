8e5878e7dc32c3a70409ca40452e728b659cd1f6
Marco Aring <contato@marcoaring.com.br>
2021-04-28 21:50:51 -0300

    feat: create logic to change styles of pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Redirect, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 
-import AboutPage from './pages/about/';
+import CartPage from './pages/cart';
+import ElectricPage from './pages/electric';
+import ErrorPage from './pages/404';
+import FirePage from './pages/fire';
 import MainPage from './pages/home/';
+import WaterPage from './pages/water';
+
+import { themes } from './styles/global';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,9 +19,69 @@ ReactDOM.render(
         <Route path="/" exact>
           <MainPage />
         </Route>
-        <Route path="/sobre" exact>
-          <AboutPage />
+        <Route
+          path="/agua"
+          exact
+          render={() => {
+            const currentTheme = themes.find((theme) => theme.name === 'water');
+            localStorage.setItem('THEME', currentTheme?.name || '');
+
+            return (
+              <ThemeProvider theme={currentTheme}>
+                <WaterPage />
+              </ThemeProvider>
+            );
+          }}
+        />
+        <Route
+          path="/eletrico"
+          exact
+          render={() => {
+            const currentTheme = themes.find((theme) => theme.name === 'electric');
+            localStorage.setItem('THEME', currentTheme?.name || '');
+
+            return (
+              <ThemeProvider theme={currentTheme}>
+                <ElectricPage />
+              </ThemeProvider>
+            );
+          }}
+        />
+        <Route
+          path="/fogo"
+          exact
+          render={() => {
+            const currentTheme = themes.find((theme) => theme.name === 'fire');
+            localStorage.setItem('THEME', currentTheme?.name || '');
+
+            return (
+              <ThemeProvider theme={currentTheme}>
+                <FirePage />
+              </ThemeProvider>
+            );
+          }}
+        />
+        <Route
+          path="/carrinho"
+          exact
+          render={() => {
+            const currentTheme = localStorage.getItem('THEME') || '';
+
+            if (currentTheme) {
+              return (
+                <ThemeProvider theme={themes.find((theme) => theme.name === currentTheme)}>
+                  <CartPage />
+                </ThemeProvider>
+              );
+            } else {
+              return <Redirect to="/404" />;
+            }
+          }}
+        />
+        <Route path="/404" exact>
+          <ErrorPage />
         </Route>
+        <Route path="*" exact render={() => <Redirect to="/404" />} />
       </Switch>
     </Router>
   </React.StrictMode>,
