69796aa86a970f4b19abf6ff99266758ce06417d
Marco Aring <contato@marcoaring.com.br>
2021-04-28 21:49:26 -0300

    style: create a global style for the three styles

---
+++ src/styles/global.ts
@@ -0,0 +1,14 @@
+export const themes = [
+  {
+    name: 'electric',
+    bg: '#efba00',
+  },
+  {
+    name: 'fire',
+    bg: '#ef0000',
+  },
+  {
+    name: 'water',
+    bg: '#00aeef',
+  },
+];
