30a856d41f303fd788d61c602a95037de4e867d2
Marco Aring <contato@marcoaring.com.br>
2021-04-30 18:51:55 -0300

    feat: insert header in pages

--- src/pages/fire/index.tsx
+++ src/pages/fire/index.tsx
@@ -1,13 +1,11 @@
+import { Header } from '../../components';
 import * as S from './styles';
 
 const FirePage = () => {
   return (
-    <>
-      <S.Box>This is the about page, where I tell you all about myself!</S.Box>
-      <a className="App-link" href="/">
-        Home
-      </a>
-    </>
+    <S.Main>
+      <Header />
+    </S.Main>
   );
 };
 
--- src/pages/fire/styles.ts
+++ src/pages/fire/styles.ts
@@ -1,7 +1,3 @@
 import styled from 'styled-components';
 
-export const Box = styled.p`
-  width: 100%;
-  height: 100%;
-  background-color: ${(props) => props.theme.bg};
-`;
+export const Main = styled.main``;
