e977fb9f4d7c77a3bf8b48026f140ec9e493e6c6
Marco Aring <contato@marcoaring.com.br>
2021-04-30 11:04:03 -0300

    feat: create home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,21 +1,48 @@
-import styled from 'styled-components';
-
-const MainText = styled.p`
-  display: flex;
-  flex: 1;
-  background-color: white;
-  justify-content: center;
-  align-items: center;
-`;
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import * as S from './styles';
 
 const MainPage = () => {
+  const [magicLink, changeMagicLink] = useState(false);
+  const history = useHistory();
+
+  const randomClass = () => {
+    const randomOptions = ['agua', 'fogo', 'eletrico'];
+    const randomPosition = Math.floor(Math.random() * 3);
+
+    history.push(`/${randomOptions[randomPosition]}`);
+  };
+
   return (
-    <>
-      <MainText>This is the about page, where I tell you all about myself!</MainText>
-      <a className="App-link" href="/sobre">
-        Sobre
-      </a>
-    </>
+    <S.Main>
+      <S.Logo src="logo-pokestore.png" alt="PokeStore" title="Pokestore" />
+
+      <S.Informative>
+        Olá <S.Strong>treinador</S.Strong>, seja bem vindo a experiência{' '}
+        <S.Strong>PokeStore</S.Strong>.
+      </S.Informative>
+
+      <S.Informative>
+        Abaixo você encontrará um botão que irá gerar automaticamente e randomicamente uma das três
+        classes principais do mundo pokémon: <S.Strong>Água</S.Strong>, <S.Strong>Fogo</S.Strong> ou{' '}
+        <S.Strong>Elétrico</S.Strong>.
+      </S.Informative>
+
+      <S.Button onClick={() => randomClass()}>Iniciar</S.Button>
+      <S.Informative>
+        Mas, calma, caso deseje escolher aquela classe de sua preferência, você também pode. Basta
+        clicar nesse{' '}
+        <S.MagicLink onClick={() => changeMagicLink(!magicLink)}>link mágico</S.MagicLink>.
+      </S.Informative>
+
+      {magicLink && (
+        <S.Informative>
+          <S.RedirectLink to="/agua">Água</S.RedirectLink> |{' '}
+          <S.RedirectLink to="/fogo">Fogo</S.RedirectLink> |{' '}
+          <S.RedirectLink to="/eletrico">Elétrico</S.RedirectLink>
+        </S.Informative>
+      )}
+    </S.Main>
   );
 };
 
---
+++ src/pages/home/styles.ts
@@ -0,0 +1,85 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import media from 'styled-media-query';
+
+export const Main = styled.main`
+  align-items: center;
+  background-color: #e1e1e1;
+  display: flex;
+  flex-flow: column;
+  justify-content: center;
+  min-height: 100vh;
+  width: 100%;
+`;
+
+export const Logo = styled.img`
+  margin-bottom: 30px;
+  max-height: 120px;
+  max-width: 454px;
+  width: 90%;
+
+  ${media.greaterThan('medium')`
+    width: 100%
+  `}
+`;
+
+export const Informative = styled.p`
+  font-size: 17px;
+  letter-spacing: 1px;
+  margin-bottom: 15px;
+  max-width: 85%;
+  text-align: center;
+
+  ${media.greaterThan('medium')`
+    max-width: 400px;
+  `}
+`;
+
+export const Strong = styled.strong`
+  font-weight: bold;
+`;
+
+export const Button = styled.button`
+  background-color: #386abb;
+  border: 2px solid #386abb;
+  color: #ffffff;
+  letter-spacing: 1px;
+  margin: 15px 0 30px;
+  max-width: 85%;
+  padding: 8px 0;
+  text-transform: uppercase;
+  transition: all 0.3s linear;
+  width: 70%;
+
+  &:hover {
+    background-color: transparent;
+    color: #000000;
+    cursor: pointer;
+  }
+
+  ${media.greaterThan('medium')`
+    max-width: 360px;
+    padding: 15px 0;
+  `}
+`;
+
+export const MagicLink = styled.a`
+  color: #386abb;
+  text-decoration: underline;
+  transition: color 0.3s linear;
+
+  &:hover {
+    color: #000000;
+    cursor: pointer;
+  }
+`;
+
+export const RedirectLink = styled(Link)`
+  color: #386abb;
+  text-decoration: none;
+  transition: color 0.3s linear;
+
+  &:hover {
+    color: #000000;
+  }
+`;
