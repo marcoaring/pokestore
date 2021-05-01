b60e0df1c9bd5a2b3527702a89d9451a00b20d62
Marco Aring <contato@marcoaring.com.br>
2021-05-01 11:29:30 -0300

    feat: insert service for get poke in water page

--- src/pages/water/index.tsx
+++ src/pages/water/index.tsx
@@ -1,11 +1,34 @@
-import { Header } from '../../components';
+import { useEffect, useState } from 'react';
+
+import { Header, Loading, Pokemon } from '../../components';
+
+import * as FetchService from '../../services/fetch';
+
 import * as S from './styles';
 
 const WaterPage = () => {
+  const [loading, changeLoading] = useState(true);
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    FetchService.getPokemons('water').then((response) => {
+      setPokemons(response);
+      changeLoading(false);
+    });
+  }, []);
+
   return (
-    <S.Main>
+    <>
       <Header />
-    </S.Main>
+      <S.Wrapper>
+        {!loading &&
+          pokemons.length > 0 &&
+          pokemons.map((pokemon: any) => {
+            return <Pokemon pokemon={pokemon} key={pokemon.id} />;
+          })}
+        {loading && <Loading />}
+      </S.Wrapper>
+    </>
   );
 };
 
--- src/pages/water/styles.ts
+++ src/pages/water/styles.ts
@@ -1,3 +1,25 @@
 import styled from 'styled-components';
+import media from 'styled-media-query';
 
-export const Main = styled.main``;
+export const Wrapper = styled.div`
+  align-items: center;
+  display: flex;
+  flex-wrap: wrap;
+  height: 100%;
+  justify-content: center;
+  padding: 1em;
+  text-align: center;
+
+  ${media.between('medium', 'large')`
+    max-width: 95%;
+  `}
+
+  ${media.greaterThan('medium')`
+    margin: 0 auto;
+    padding: 1.2em 0;
+  `}
+
+  ${media.greaterThan('large')`
+    max-width: 80%;
+  `}
+`;
