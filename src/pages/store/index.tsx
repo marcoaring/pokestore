b6e64e1b19d673348e7030cebf206cebeeffd63e
Marco Aring <contato@marcoaring.com.br>
2021-05-01 17:43:14 -0300

    feat: make pagination work

--- src/pages/store/index.tsx
+++ src/pages/store/index.tsx
@@ -1,39 +1,48 @@
 import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 import { Header, Loading, Navigation, Pokemon } from '../../components';
 
 import * as FetchService from '../../services/fetch';
+
+import IParams from './index.interface';
 
 import * as S from './styles';
 
 const StorePage = () => {
   const [loading, changeLoading] = useState(true);
   const [pokemons, setPokemons] = useState([]);
+  const params = useParams<IParams>();
   const type = localStorage.getItem('THEME') || '';
+  const currentPagination = params ? parseInt(params.page || '1') : 1;
 
   useEffect(() => {
-    FetchService.getPokemons(type).then((response) => {
+    changeLoading(true);
+    FetchService.getPokemons(type, currentPagination).then((response) => {
       setPokemons(response);
       changeLoading(false);
     });
-  }, [type]);
+  }, [type, currentPagination]);
 
   return (
     <>
       <Header />
       <S.Wrapper>
         {!loading && pokemons.length > 0 && (
-          <S.Pokemons>
-            {pokemons.map((pokemon: any) => {
-              return <Pokemon pokemon={pokemon} key={pokemon.id} />;
-            })}
-          </S.Pokemons>
+          <>
+            <S.Pokemons>
+              {pokemons.map((pokemon: any) => {
+                return <Pokemon pokemon={pokemon} key={pokemon.id} />;
+              })}
+            </S.Pokemons>
+
+            <Navigation
+              currentPagination={currentPagination}
+              pagination={parseInt(sessionStorage.getItem('PAGINATION') || '1')}
+            />
+          </>
         )}
 
-        <Navigation
-          currentPagination={parseInt(sessionStorage.getItem('CURRENT_PAGINATION') || '1')}
-          pagination={parseInt(sessionStorage.getItem('PAGINATION') || '1')}
-        />
         {loading && <Loading />}
       </S.Wrapper>
     </>
