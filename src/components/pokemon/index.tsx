de7d2f86446fbb31090e2f8584e17cd3b0f4a6d2
Marco Aring <contato@marcoaring.com.br>
2021-05-01 15:19:52 -0300

    feat: create card of pokemon

--- src/components/pokemon/index.tsx
+++ src/components/pokemon/index.tsx
@@ -3,5 +3,30 @@ import IPokemon from './index.interface';
 import * as S from './styles';
 
 export function Pokemon({ pokemon }: IPokemon) {
-  return <S.Pokemon>{pokemon.name}</S.Pokemon>;
+  const addToPokedex = (id: number) => {
+    //Function to add pokemon into pokedex
+  };
+
+  return (
+    <S.PokemonCard>
+      <S.PokemonImage>
+        <S.LinkDescription to={`/pokemon/${pokemon.slug}`}>
+          <S.Image src={pokemon.image} alt={pokemon.name} title={pokemon.name} />
+        </S.LinkDescription>
+      </S.PokemonImage>
+      <S.PokemonInfos>
+        <S.LinkDescription to={`/pokemon/${pokemon.slug}`}>
+          <S.Info>{pokemon.name}</S.Info>
+        </S.LinkDescription>
+        <S.Info>
+          {pokemon.price.toLocaleString('pt-br', {
+            style: 'currency',
+            currency: 'BRL',
+          })}
+        </S.Info>
+      </S.PokemonInfos>
+
+      <S.Button onClick={() => addToPokedex(pokemon.id)}>Adicionar na Pokedex</S.Button>
+    </S.PokemonCard>
+  );
 }
--- src/components/pokemon/styles.ts
+++ src/components/pokemon/styles.ts
@@ -1,4 +1,82 @@
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-// import media from 'styled-media-query';
+import media from 'styled-media-query';
 
-export const Pokemon = styled.div``;
+export const PokemonCard = styled.div`
+  background-color: #ffffff;
+  box-shadow: rgb(0 0 0 / 20%) 0px 2px 6px;
+  color: #666666;
+  margin: 15px;
+  padding: 15px;
+  width: 98%;
+
+  ${media.between('medium', 'large')`
+    flex: 1 0 41%;
+  `}
+
+  ${media.greaterThan('large')`
+    flex: 1 0 21%;
+  `}
+`;
+
+export const PokemonImage = styled.div`
+  margin-bottom: 15px;
+  text-align: center;
+`;
+
+export const LinkDescription = styled(Link)`
+  color: #666666;
+  text-decoration: none;
+  transition: all 0.3s linear;
+
+  img {
+    transition: all 0.3s linear;
+
+    &:hover {
+      opacity: 0.7;
+    }
+  }
+
+  &:hover {
+    color: ${(props) => props.theme.bg};
+  }
+`;
+
+export const Image = styled.img`
+  display: block;
+  height: auto;
+  margin: 0 auto;
+  max-width: 100%;
+
+  ${media.greaterThan('medium')`
+    height: 181px;
+  `}
+`;
+
+export const PokemonInfos = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 30px;
+`;
+
+export const Info = styled.span`
+  font-size: 20px;
+  font-weight: bold;
+`;
+
+export const Button = styled.button`
+  background-color: ${(props) => props.theme.bg};
+  border: 2px solid ${(props) => props.theme.bg};
+  color: #ffffff;
+  letter-spacing: 1px;
+  padding: 15px 0;
+  text-transform: uppercase;
+  transition: all 0.3s linear;
+  width: 100%;
+
+  &:hover {
+    background-color: transparent;
+    color: ${(props) => props.theme.bg};
+    cursor: pointer;
+  }
+`;
