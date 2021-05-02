fe87d5c83d29f99a679d6ad7075acdd0198e2ab4
Marco Aring <contato@marcoaring.com.br>
2021-05-02 17:32:41 -0300

    feat: create pokedex in header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,4 +1,6 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+
+import { usePokedex } from '../../hooks/pokedex';
 
 import { Icon } from '../icon';
 
@@ -7,12 +9,15 @@ import * as S from './styles';
 import logo from '../../assets/logo-pokestore.png';
 
 export function Header() {
+  const [open, changeOpen] = useState(false);
+  const { count, pokedex, total } = usePokedex();
+
   return (
     <S.Header>
       <S.Wrapper>
-        <Link to="/">
+        <S.LinkHome to="/">
           <S.Logo src={logo} alt="Logo PokeStore" title="Logo PokeStore" />
-        </Link>
+        </S.LinkHome>
 
         <S.Form>
           <S.Input type="text" name="search" placeholder="Busque seu Pokémon" autoComplete="off" />
@@ -21,10 +26,58 @@ export function Header() {
           </S.Button>
         </S.Form>
 
-        <S.Pokedex>
-          <S.Count>0</S.Count>
+        <S.WrapperPokedex onClick={() => changeOpen(!open)}>
+          <S.Count>{count}</S.Count>
           <Icon name="icon-pokedex" />
-        </S.Pokedex>
+
+          {open && (
+            <S.Pokedex className={pokedex.length > 0 ? '' : '-emptyPokedex'}>
+              <S.Items>
+                <S.Title className={pokedex.length > 0 ? '' : '-emptyPokedex'}>
+                  {pokedex.length > 0 ? 'Minha Pokedex' : 'Sua Pokedex está vazia'}
+                </S.Title>
+
+                {pokedex.length > 0 && (
+                  <S.Pokemons>
+                    {pokedex.map((pokemon: IPokemon) => {
+                      return (
+                        <S.Pokemon key={pokemon.id}>
+                          <S.LinkRedirect to={`/pokemon/${pokemon.slug}`}>
+                            <S.Image src={pokemon.image} alt={pokemon.name} title={pokemon.name} />
+                            <S.Infos>
+                              <S.Name>{pokemon.name}</S.Name>
+                              <S.Quantity>Quantidade: {pokemon.quantity}</S.Quantity>
+                            </S.Infos>
+                            <S.Price>
+                              {pokemon.price.toLocaleString('pt-br', {
+                                style: 'currency',
+                                currency: 'BRL',
+                              })}
+                            </S.Price>
+                          </S.LinkRedirect>
+                        </S.Pokemon>
+                      );
+                    })}
+                  </S.Pokemons>
+                )}
+              </S.Items>
+
+              {pokedex.length > 0 && total > 0 && (
+                <S.Finish>
+                  <S.TotalPrice>
+                    Total:{' '}
+                    {total.toLocaleString('pt-br', {
+                      style: 'currency',
+                      currency: 'BRL',
+                    })}
+                  </S.TotalPrice>
+
+                  <S.FinishButton to="/pokedex">Catch 'Em All</S.FinishButton>
+                </S.Finish>
+              )}
+            </S.Pokedex>
+          )}
+        </S.WrapperPokedex>
       </S.Wrapper>
     </S.Header>
   );
--- src/components/header/styles.ts
+++ src/components/header/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 import media from 'styled-media-query';
 
 export const Header = styled.header`
@@ -30,15 +31,18 @@ export const Wrapper = styled.div`
   `}
 `;
 
-export const Logo = styled.img`
-  margin-bottom: 15px;
+export const LinkHome = styled(Link)`
   width: 70%;
   order: 1;
 
   ${media.greaterThan('medium')`
-    margin-bottom: 0;
     width: 232px;
   `}
+`;
+
+export const Logo = styled.img`
+  margin-bottom: 15px;
+  width: 100%;
 `;
 
 export const Form = styled.form`
@@ -82,7 +86,7 @@ export const Button = styled.button`
   }
 `;
 
-export const Pokedex = styled.div`
+export const WrapperPokedex = styled.div`
   margin-top: -19px;
   order: 2;
   position: relative;
@@ -118,4 +122,128 @@ export const Count = styled.span`
   position: absolute;
   right: -8px;
   top: -10px;
+`;
+
+export const Pokedex = styled.div`
+  background-color: #ffffff;
+  border: 2px solid ${(props) => props.theme.bg};
+  color: #666666;
+  max-width: 23.125em;
+  min-width: 18.125em;
+  position: absolute;
+  right: -15px;
+  text-align: left;
+  top: 150%;
+  width: 300px;
+  z-index: 1;
+
+  &.-emptyPokedex {
+    align-items: center;
+    display: flex;
+    justify-content: center;
+  }
+
+  &:before {
+    border-bottom: 12px solid #fff;
+    border-left: 12px solid transparent;
+    border-right: 12px solid transparent;
+    content: '';
+    height: 0;
+    position: absolute;
+    right: 20px;
+    top: -12px;
+    width: 0;
+  }
+`;
+
+export const Items = styled.div`
+  padding: 15px;
+`;
+
+export const Title = styled.h3`
+  border-bottom: 1px solid ${(props) => props.theme.bg};
+  color: ${(props) => props.theme.bg};
+  font-size: 16px;
+  font-weight: 600;
+  padding-bottom: 5px;
+
+  &.-emptyPokedex {
+    border: none;
+    color: #666666;
+  }
+`;
+
+export const Pokemons = styled.ul`
+  margin-top: 15px;
+  list-style: none;
+`;
+
+export const Pokemon = styled.li`
+  margin-bottom: 15px;
+
+  &:last-child {
+    margin-bottom: 0;
+  }
+`;
+
+export const LinkRedirect = styled(Link)`
+  align-items: center;
+  color: #666666;
+  display: flex;
+  justify-content: space-between;
+  text-decoration: none;
+`;
+
+export const Image = styled.img`
+  max-width: 2.5em;
+  margin-right: 15px;
+`;
+
+export const Infos = styled.div`
+  margin-right: 30px;
+`;
+
+export const Name = styled.p`
+  font-weight: bold;
+`;
+
+export const Quantity = styled.span`
+  font-size: 12px;
+`;
+
+export const Price = styled.span`
+  font-weight: bold;
+`;
+
+export const Finish = styled.div`
+  align-items: flex-end;
+  background-color: #f2f2f2;
+  display: flex;
+  flex-flow: column;
+  justify-content: flex-end;
+  padding: 15px;
+`;
+
+export const TotalPrice = styled.p`
+  font-weight: bold;
+  text-align: right;
+  margin-bottom: 15px;
+`;
+
+export const FinishButton = styled(Link)`
+  background-color: ${(props) => props.theme.bg};
+  border: 2px solid ${(props) => props.theme.bg};
+  color: #ffffff;
+  font-weight: bold;
+  letter-spacing: 1px;
+  padding: 5px 15px;
+  text-decoration: none;
+  text-transform: uppercase;
+  transition: all 0.3s linear;
+
+  &:hover {
+    background-color: transparent;
+    color: ${(props) => props.theme.bg};
+    cursor: pointer;
+  }
 `;
