f56ce70382b37ed0a1c0e7ee297682e441257ed2
Marco Aring <contato@marcoaring.com.br>
2021-05-02 17:30:01 -0300

    feat: set default interface for Pokemon

---
+++ src/type.d.ts
@@ -0,0 +1,11 @@
+interface IPokemon {
+  base_experience: number;
+  height: number;
+  id: number;
+  image: string;
+  name: string;
+  price: number;
+  quantity: number;
+  slug: string;
+  weight: number;
+}
