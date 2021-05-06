d19e83c9fe718fc56e42efda755aa27621ccbf87
Marco Aring <contato@marcoaring.com.br>
2021-05-05 21:23:44 -0300

    test: insert mocks for unit tests

---
+++ src/mocks/mockPokemon.ts
@@ -0,0 +1,25 @@
+export const mockPokemon = {
+  base_experience: 62,
+  height: 6,
+  id: 4,
+  image:
+    'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/4.svg',
+  name: 'Charmander',
+  price: 20.67,
+  quantity: 1,
+  slug: 'charmander',
+  weight: 85,
+};
+
+export const mockNewPokemon = {
+  base_experience: 112,
+  height: 4,
+  id: 25,
+  image:
+    'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/25.svg',
+  name: 'Pikachu',
+  price: 37.33,
+  quantity: 1,
+  slug: 'pikachu',
+  weight: 60,
+};
