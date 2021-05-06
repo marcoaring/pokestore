c5a4e57adc1689d07a5047a481a75442324003ae
Marco Aring <contato@marcoaring.com.br>
2021-05-06 10:35:00 -0300

    test: insert unit test for home page

--- src/pages/home/index.test.tsx
+++ src/pages/home/index.test.tsx
@@ -1,8 +1,95 @@
-import { render, screen } from '@testing-library/react';
-import MainPage from '.';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 
-test('renders learn react link', () => {
-  render(<MainPage />);
-  const linkElement = screen.getByText(/Sobre/i);
-  expect(linkElement).toBeInTheDocument();
+import MainPage from './';
+
+const mockHistoryPush = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useHistory: () => ({
+    push: mockHistoryPush,
+  }),
+}));
+
+describe('<MainPage />', () => {
+  it('should render successfully', () => {
+    render(
+      <MemoryRouter>
+        <MainPage />
+      </MemoryRouter>
+    );
+
+    expect(
+      screen.getByRole('img', {
+        name: /pokestore/i,
+      })
+    ).toBeInTheDocument();
+  });
+
+  it('should render magic link successfully', () => {
+    render(
+      <MemoryRouter>
+        <MainPage />
+      </MemoryRouter>
+    );
+
+    fireEvent.click(screen.getByText(/link mágico/i));
+
+    expect(
+      screen.getByRole('button', {
+        name: /água/i,
+      })
+    ).toBeInTheDocument();
+
+    expect(
+      screen.getByRole('button', {
+        name: /fogo/i,
+      })
+    ).toBeInTheDocument();
+
+    expect(
+      screen.getByRole('button', {
+        name: /elétrico/i,
+      })
+    ).toBeInTheDocument();
+  });
+
+  it('should render random class redirect successfully', () => {
+    render(
+      <MemoryRouter>
+        <MainPage />
+      </MemoryRouter>
+    );
+
+    fireEvent.click(
+      screen.getByRole('button', {
+        name: /iniciar/i,
+      })
+    );
+
+    expect(mockHistoryPush).toHaveBeenCalledWith('/loja');
+  });
+
+  it('should render magic link redirect successfully', () => {
+    render(
+      <MemoryRouter>
+        <MainPage />
+      </MemoryRouter>
+    );
+
+    fireEvent.click(screen.getByText(/link mágico/i));
+
+    fireEvent.click(screen.getByRole('button', { name: /água/i }));
+
+    expect(mockHistoryPush).toHaveBeenCalledWith('/loja');
+
+    fireEvent.click(screen.getByRole('button', { name: /fogo/i }));
+
+    expect(mockHistoryPush).toHaveBeenCalledWith('/loja');
+
+    fireEvent.click(screen.getByRole('button', { name: /elétrico/i }));
+
+    expect(mockHistoryPush).toHaveBeenCalledWith('/loja');
+  });
 });
